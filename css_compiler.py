1c9d9b16ccc4b150193ed6a368bb20d938dba11b
bth123 <133943325+bth123@users.noreply.github.com>
2024-01-17 21:29:10 +0200

    latest one

--- css_compiler.py
+++ css_compiler.py
@@ -1,9 +1,27 @@
 import os
+from urllib.parse import quote
+from pyperclip import copy
 
 templates = {
+	"metadata": """
+/**
+ * @name New emojis
+ * @author bth123
+ * @authorId 567014541507035148
+ * @version 1.0
+ * @description ✨ Theme with new emojis from message bar
+ * @website https://github.com/bth123/new-ds-emojis
+ * **/
+ """,
+
 	"chat": """
-.emoji[data-name=":{name}:"] {{
-  content: url("https://github.com/bth123/new-ds-emojis/blob/9fb4e51a2a6fc2bf68e77959f796311790d8f468/emojis/{name}.png?raw=true");
+.emoji[data-name=":{name}:"], .emoji[data-name="{associated_emoji}"] {{
+  content: url("https://github.com/bth123/new-ds-emojis/blob/main/emojis/{link_name}.png?raw=true");
+}}""",
+
+	"reaction_list": """
+.emoji__32c91[alt="{associated_emoji}"] {{
+	content: url("https://github.com/bth123/new-ds-emojis/blob/main/emojis/{link_name}.png?raw=true");
 }}""",
 
 	"emoji_menu":"""
@@ -14,32 +32,39 @@ templates = {
   background-size: 45px, 45px;
   background-position: center;
   background-repeat: no-repeat;
-  background-image: url("https://github.com/bth123/new-ds-emojis/blob/9fb4e51a2a6fc2bf68e77959f796311790d8f468/emojis/{name}.png?raw=true");
+  background-image: url("https://github.com/bth123/new-ds-emojis/blob/main/emojis/{link_name}.png?raw=true");
 }}""",
 	
 	"emoji_menu_preview": """
 .emoji__92b46[alt=":{name}:"] {{
-  content: url("https://github.com/bth123/new-ds-emojis/blob/9fb4e51a2a6fc2bf68e77959f796311790d8f468/emojis/{name}.png?raw=true");
+  content: url("https://github.com/bth123/new-ds-emojis/blob/main/emojis/{link_name}.png?raw=true");
 }}""",
 
 	"emoji_menu_preview_multiple_variants": """
 .emoji__92b46[alt=":{names}:"] {{
-  content: url("https://github.com/bth123/new-ds-emojis/blob/9fb4e51a2a6fc2bf68e77959f796311790d8f468/emojis/{name}.png?raw=true");
+  content: url("https://github.com/bth123/new-ds-emojis/blob/main/emojis/{link_name}.png?raw=true");
 }}"""
 }
 
-try:
-	files = os.listdir("emojis")
-	
-	png_files = [file for file in files if file.lower().endswith('.png')]
-	with open("theme.css", "a") as theme:
-		for png_file in png_files:
-			png_name = png_file[:-4].split(" ")[0]
-			theme.write(templates["chat"].format(name=png_name))
-			theme.write(templates["emoji_menu"].format(name=png_name))
-			if " " in png_file:
-				theme.write(templates["emoji_menu_preview_multiple_variants"].format(name=png_name, names=": :".join(png_file[:-4].split(" "))))
-			else:
-				theme.write(templates["emoji_menu_preview"].format(name=png_name))
-except:
-	print("saygex")
+files = os.listdir("emojis")
+
+png_files = [file for file in files if file.lower().endswith('.png')]
+with open("theme.css", "w", encoding="utf-8") as theme:
+	theme.write(templates["metadata"])
+with open("theme.css", "a", encoding="utf-8") as theme:
+	for png_file in png_files:
+		# Seting up vars
+		png_name = png_file[:-5].split(" ")[0]
+		associated_emoji = png_file[-5:-4]
+		link_name = quote(png_file[:-4])
+		# Compiling css
+		theme.write(templates["chat"].format(name=png_name, associated_emoji=associated_emoji, link_name=link_name))
+		theme.write(templates["reaction_list"].format(associated_emoji=associated_emoji, link_name=link_name))
+		theme.write(templates["emoji_menu"].format(name=png_name, link_name=link_name))
+		if " " in png_file:
+			theme.write(templates["emoji_menu_preview_multiple_variants"].format(name=png_name, link_name=link_name, names=": :".join(png_file[:-5].split(" "))))
+		else:
+			theme.write(templates["emoji_menu_preview"].format(name=png_name, link_name=link_name))
+
+with open("theme.css", "r", encoding="utf-8") as theme:
+	copy(theme.read())
